46789f0247e6d2c850edf77145a9484d951671c7
kaiosatiro <kaiosatiro@gmail.com>
2024-11-22 16:16:09 -0300

    first attempt

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,7 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+pubkey='xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2'
+
+descriptor_info=$(bitcoin-cli getdescriptorinfo "tr(${pubkey}/*)")
+descriptor=$(echo $descriptor_info | jq -r '.descriptor')
+bitcoin-cli deriveaddresses $descriptor "[100,100]" | jq -r .[0]
--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,21 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+# set -x
+tx_id=e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163
+
+tx=$(bitcoin-cli getrawtransaction $tx_id true | jq -c '.vin[0]')
+tx_wit_script=$(echo $tx | jq -r '.txinwitness[-1]')
+
+script=$(bitcoin-cli decodescript $tx_wit_script | jq -r '.asm')
+
+declare -i i=0
+for item in $script;
+do
+    if [ "$i" -eq 1 ];
+    then
+        echo $item
+        break
+    fi
+    i+=1
+done
+
